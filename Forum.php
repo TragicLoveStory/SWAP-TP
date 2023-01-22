513b4ca76cf8e865f5704e65525e44976ca5e59e
Jreenk <92773641+Jreenk@users.noreply.github.com>
2023-01-23 05:27:28 +0800

    Forum CSS almost done
    Can't figure out how to set the footer for forumThread and removing the prompt for connecting to 127.0.0.1

--- Forum.php
+++ Forum.php
@@ -5,9 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
+
     
     <?php 
     require "config.php";
@@ -19,7 +23,8 @@
             header("Location: http://localhost/SWAP-TP/createThread.php");
             die();
         }
-        echo '<form action="Forum.php" method="POST"><input type="submit" value="Create a forum post" name="createForumPost"></form>';
+        include "navbar.php";
+        echo '<br><form action="Forum.php" method="POST"><input type="submit" value="Create a forum post" name="createForumPost" ></form><br><br>';
         //connection to internalhr database
         try {
             $con=mysqli_connect($db_hostname,$db_username,$db_password,$db_database);
@@ -31,19 +36,33 @@
                 printerror("Connecting to $db_hostname", $con);
                 die();
             }
-        else printok("Connecting to $db_hostname");
+            //else printok("Connecting to $db_hostname");
+
+
         // loading of forum details
         $query=$con->prepare("SELECT forum.id, users.email, forum.title, forum.createOn, forum.lastEdited, forum.viewCount, forum.status FROM forum INNER JOIN users ON forum.userId = users.ID");
         $query->execute();
             $query->bind_result($id, $email, $title, $createOn, $lastEdited, $viewCount,$status);
-            echo "<table align='center' border='1'><tr>";
-            echo
-            "<th>email</th><th>title</th><th>createOn</th><th>lastEdited</th><th>viewCount</th></tr>";
+            echo "<table class = 'table'
+            
+            align='center' 
+            border='0' 
+            height='400' 
+            WIDTH='1500'><tr>";
+
+
+
+            echo "<th>email</th>
+                  <th>title</th>
+                  <th>createOn</th>
+                  <th>lastEdited</th>
+                  <th>viewCount</th></tr>";
             while($query->fetch())
             {
                 echo "<tr><th>$email</th><th><a href='forumThread.php?forumID=".$id."'>$title</a></th><th>$createOn</th><th>$lastEdited</th><th>$viewCount</th></tr>";
             }
-            echo "</table>";
+            echo "</table><br><br>";
+
     }
     elseif(isset($_SESSION["ID"]) && isset($_SESSION["role"]) && $_SESSION["role"] === "FORUM-ADMIN"){
         if (isset($_POST['deletion']) && $_POST['deletion'] === 'Delete') {
@@ -95,7 +114,6 @@
         $query=$con->prepare("SELECT forum.id, users.email, forum.title, forum.createOn, forum.lastEdited, forum.viewCount, forum.status FROM forum INNER JOIN users ON forum.userId = users.ID");
         $query->execute();
             $query->bind_result($id, $email, $title, $createOn, $lastEdited, $viewCount,$status);
-            echo "<table align='center' border='1'><tr>";
             echo
             "<th>id</th><th>email</th><th>title</th><th>createOn</th><th>lastEdited</th><th>viewCount</th><th>Status</th></tr>";
             while($query->fetch())
@@ -115,13 +133,25 @@
         echo "Must be logged in.";
         die();
     }
+    include "footer.php";
     ?>
+
+
     <style>
         th{
-            max-width: 150px;
+            max-width: 500px;
             text-overflow: ellipsis;
             overflow: hidden;
             white-space: nowrap;
+            text-align: center;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        form{
+            float: right;
+            padding-right: 10px;
         }
     </style>
 </body>
