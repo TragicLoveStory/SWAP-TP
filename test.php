b2993e32bfd39420511086691c7b3d65203bf21c
TragicLoveStory <87907772+TragicLoveStory@users.noreply.github.com>
2023-01-26 15:21:38 +0800

    added navbar and footer to contactUs.php

--- test.php
+++ test.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content=
-		"width=device-width, initial-scale=1.0">
-
-	<!-- CSS file -->
-	<link rel="stylesheet" href="style.css">
-
-	<!-- Google reCAPTCHA CDN -->
-	<script src=
-		"https://www.google.com/recaptcha/api.js" async defer>
-	</script>
-</head>
-
-<body>
-	<div class="container">
-		<h1>Google reCAPTCHA</h1>
-
-		<!-- HTML Form -->
-		<form action="action.php" method="post">
-			<input type="text" name="name" id="name"
-				placeholder="Enter Name" required>
-			<br>
-
-			<!-- div to show reCAPTCHA -->
-			<div class="g-recaptcha"
-				data-sitekey="6Lc9GR0kAAAAABFRWW083X5exGwGO2CgFxbR8G6b">
-			</div>
-			<br>
-
-			<button type="submit" name="submit_btn">
-				Submit
-			</button>
-		</form>
-	</div>
-</body>
-
-</html>
+<?php 
+use PragmaRX\Google2FA\Google2FA;
+$google2fa = new Google2FA();
+$secret = $google2fa->generateSecretKey();
+return $google2fa->generateSecretKey();
+?>
